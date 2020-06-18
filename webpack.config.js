f5aacdb81ae8eb85da0eab829b9ec397aecb4533
wenchao.zhang_U+ <wenchao.zhang@united-imaging.com>
2020-06-18 19:39:44 +0800

    xx

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin") // 压缩css代码
+const webpack = require('webpack')
 
 const path = require('path')
 const resolve = dir => path.resolve(__dirname, dir);
@@ -63,7 +64,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/seach.html',
       filename: 'index.html', //打包后的文件名
-      chunks: ['index']  //只引入index js
+      chunks: ['index']  //只引入index js 不写的话就会把所有打包的js引入
     }),
     new HtmlWebpackPlugin({
       template: './public/seach.html',
@@ -74,5 +75,10 @@ module.exports = {
       filename: 'css/[name].css',
       allChunks: true
     }),
-  ]
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
 }
